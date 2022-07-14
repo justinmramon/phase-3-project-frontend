cee734422a77380178412e55116a7f4ddfebbfc2
James Kyan <james.kyan88@gmail.com>
2022-07-14 13:20:28 -0400

    least updated, no navbar menu

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -20,11 +20,13 @@ function UserForm({allUsers, setAllUsers}) {
     }
 
     return (
-        <div>
-            <h3>Create New User</h3>
+        <div class="max-w-xs rounded overflow-hidden shadow-lg mt-8">
+            <h3 class=" text-blue-500 text-2xl mt-6 mx-14">Create New User</h3>
+            <br></br>
             <form onSubmit={handleSubmit}>
-                <input placeholder="Name" onChange={(e) => setNewUser(e.target.value)}></input>
-                <button>Create</button>
+                <input class="mx-14 block px-3 py-2 border-0 border-b-2 border-blue-300 focus:ring-0 focus:border-blue text-blue-500 placeholder-blue-500 placeholder-opacity-50 " type="text" placeholder="Name" onChange={(e) => setNewUser(e.target.value)}></input>
+                <br></br>
+                <button class="bg-transparent hover:bg-blue-400 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mx-16 mb-8">Create</button>
             </form>
         </div>
         
